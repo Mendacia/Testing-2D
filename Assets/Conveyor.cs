d43d0ce65931f019ae586ac4246d32209909f281
Liam <liamh2000@gmail.com>
2021-03-15 22:13:07 +1300

    Conveyors and Moving Platforms

---
+++ Assets/Conveyor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    private Animator anim;
+    [SerializeField] private float speed;
+    [SerializeField] private bool goingRight = true;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        var pos = myRigidbody.position;
+
+        if (goingRight)
+        {
+            anim.SetBool("Right", true);
+            myRigidbody.position += Vector2.left * speed * Time.fixedDeltaTime;
+        }
+        else
+        {
+            anim.SetBool("Right", false);
+            myRigidbody.position += Vector2.right * speed * Time.fixedDeltaTime;
+        }
+
+        myRigidbody.MovePosition(pos);
+    }
+}
---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        var pos = myRigidbody.position;
+        myRigidbody.MovePosition(pos + Vector2.right * speed * Time.fixedDeltaTime);
+    }
+}
